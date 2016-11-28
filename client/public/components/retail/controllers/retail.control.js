ccddf7e0ef4e0b6cb1da968ad4abcba19a172991
anand <anandtiwari192@gmail.com>
2016-11-28 21:10:18 +0530

    added some files

--- client/public/components/retail/controllers/retail.control.js
+++ client/public/components/retail/controllers/retail.control.js
@@ -1,4 +1,19 @@
 retail
-    .controller('RetailController', ['$scope', function($scope) {
+    .controller('RetailController', ['$scope','$http', function($scope, $http) {
         $scope.message = "Hello World";
+        
+        var config = {
+            headers : {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        };
+
+		$http.get('http://127.0.0.1:8000/chains/', config)
+        .success(function (data, status, headers, config) {
+        	console.log(data,"aa");
+        })
+        .error(function (data, status, header, config) {
+        	console.log(data,"bb");
+        });
+
 }]);
